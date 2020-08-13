fc182ee07c2f2f4f8f552c9509cbd4707a186236
Sbnc <sabriinacorrea13@hotmail.com>
2020-08-13 00:22:31 -0300

    created component select, input, textarea, layout finish...

--- web/src/components/PageHeader/index.tsx
+++ web/src/components/PageHeader/index.tsx
@@ -6,6 +6,7 @@ import './styles.css'
 
 interface PageHeaderProps {
     title: string;
+    description?: string;
 
 }
 
@@ -20,6 +21,8 @@ const PageHeader: React.FC<PageHeaderProps> = (props) => {   // React.FC = funci
             </div>
             <div className="header-content">
                 <strong>{props.title}</strong>
+                { props.description && <p>{props.description}</p>} 
+
                 {props.children}
             </div>
         </header>
