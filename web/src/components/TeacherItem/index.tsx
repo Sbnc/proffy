efc5729a3013bafe94ded4bf724482bbf941e58f
Sbnc <sabriinacorrea13@hotmail.com>
2020-08-13 23:52:14 -0300

    conection server

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,24 +3,44 @@ import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
 
 import './styles.css'
 
-function TeacherItem(){
+export interface Teacher {
+   
+    id: number;
+    avatar: string;
+    bio: string;
+    cost: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+    
+
+}
+   
+
+interface TeacherItemProps {
+    teacher: Teacher;
+
+}
+   
+
+
+
+const TeacherItem: React.FC<TeacherItemProps> = ({teacher}) =>{
     return (
         <article className='teacher-item'>
             <header>
-                <img src="https://avatars1.githubusercontent.com/u/23019379?s=460&u=8783f4865e9111d0808760aed6fe6f83b2c114a3&v=4" alt="Sabrina Corrêa"/>
+                <img src={teacher.avatar} alt={teacher.name}/>
                 <div>
-                    <strong>Sabrina Corrêa</strong>
-                    <span>Química</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header> 
             <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                <br/><br/> 
-                Veritatis dignissimos, facilis repellendus est explicabo suscipit quis! Quibusdam, culpa nesciunt tempore nisi libero architecto corrupti maxime consequuntur, explicabo ducimus expedita distinctio!
+                {teacher.bio}                
             </p>
             <footer>
                 <p>Preço/Hora
-                <strong>R$ 80,00</strong>
+                <strong>{teacher.cost}</strong>
                 </p>
                 <button type='button'>
                     <img src={whatsappIcon} alt="whatsapp"/>
